3de8be23bd951e12522e5b8267bddfefe7cc4a99
Jain-Sunaina <juug24bcas40880@jainuniversity.ac.in>
2025-05-20 18:30:17 +0530

    Add files via upload

---
+++ Insertion Sort - Part 1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void printArray(int arr[], int n) {
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+void insertionSort1(int n, int arr[]) {
+    int key = arr[n - 1];  
+    int i = n - 2;
+
+    while (i >= 0 && arr[i] > key) {
+        arr[i + 1] = arr[i];
+        printArray(arr, n);
+        i--;
+    }
+    arr[i + 1] = key;
+    printArray(arr, n);
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    insertionSort1(n, arr);
+    return 0;
+}
